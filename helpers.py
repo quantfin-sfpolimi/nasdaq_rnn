88d7bf5facec32827567fa7e816dd45c0639dc89
AlbertoToia <albetoiaa@gmail.com>
2024-03-26 22:08:44 +0100

    adding helpers.py

--- helpers.py
+++ helpers.py
@@ -1,114 +1,217 @@
-#libraries
+# Libraries used 
 import datetime as dt
-from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-from zlib import crc32
 import os
+import pandas as pd
 import pickle
 import yfinance as yf
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from keras.callbacks import History
+from zlib import crc32
 import re
 
-###pickling, both dumping and loading
-# The same functions can be used for both datset and ML model
-def pickle_dump(obj, filename):
+history = History()  # Ignore, it helps with model_data function
 
-    # Check if filename ends with "".pkl", if not add it
+
+def pickle_dump(obj, filename):
+    """
+    Serialize the given object and save it to a file using pickle.
+
+    Parameters:
+    obj:
+        anything, dataset or ML model
+    filename: str
+        The name of the file to which the object will be saved. If the filename
+        does not end with ".pkl", it will be appended automatically.
+
+    Returns:
+    None
+    """
+    # Check if filename ends with ".pkl", if not add it
     if not re.search("^.*\.pkl$", filename):
-        filename += ".pkl"       
+        filename += ".pkl"
 
     with open(filename, "wb") as f:
         pickle.dump(obj, f)
 
 
 def pickle_load(filename):
+    """
+    Load a serialized object from a file using pickle.
+
+    Parameters:
+    filename: str
+        The name of the file from which the object will be loaded. If the filename
+        does not end with ".pkl", it will be appended automatically.
+
+    Returns:
+    obj: any Python object
+        The deserialized object loaded from the file.
+    """
     if not re.search("^.*\.pkl$", filename):
         filename += ".pkl"
-    
-    try: 
-        with open(filename, "rb") as f:
-            obj = pickle.load(f)
-        except FileNotFoundError:
-            print(filename, " doesn't exist.")
+
+    with open(filename, "rb") as f:
+        obj = pickle.load(f)
 
     return obj
 
-#load data, online or from pickle
-def load_dataframe():
-    #check if pickle file exists
-    if(os.path.isfile("stocks_prices_dataframe.pkl")):
+
+def load_dataframe(years):
+    """
+    Loads stock price data either from a pickled file or downloads it online using the yfinance library.
+    Returns a pandas DataFrame containing the stock prices and a list of tickers.
+
+    Parameters:
+        years (int): Number of years of historical data to load.
+
+    Returns:
+        Tuple[pandas.DataFrame, List[str]]: A tuple containing the pandas DataFrame of stock prices and a list of tickers.
+    """
+    if os.path.isfile("stocks_prices_dataframe.pkl"):
         stock_prices = pickle_load("stocks_prices_dataframe.pkl")
-    #if pickle doesn't exist, then pick data online
+        tickers = stock_prices.columns.tolist()
     else:
-        tickers = get_nasdaq_tickers()
-        stock_prices = loaded_df(years=5, tickers)
+        tickers = get_stockex_tickers()
+        stock_prices = loaded_df(years=years, tickers=tickers)
 
-    return stock_prices
+    return stock_prices, tickers
 
 
-###hashing
 def hashing_and_splitting(adj_close_df):
-    # calculate checksum for every index
-    checksum = np.array([crc32(v) for v in adj_close_df.index.values])
-    # fraction of data to use for testing only
-    test_ratio = 0.2
-    # pick 20% of indices for testing
-    test_indices = checksum < test_ratio * 2**32
+    """
+    Splits the given DataFrame of adjusted close prices into training and testing sets based on checksum hashing.
 
+    Parameters:
+        adj_close_df (pandas.DataFrame): DataFrame containing adjusted close prices.
+
+    Returns:
+        Tuple[pandas.DataFrame, pandas.DataFrame]: A tuple containing the training and testing DataFrames.
+    """
+    checksum = np.array([crc32(v) for v in adj_close_df.index.values])
+    test_ratio = 0.2
+    test_indices = checksum < test_ratio * 2 ** 32
     return adj_close_df[~test_indices], adj_close_df[test_indices]
 
-###data cleaning
+
+def get_stockex_tickers(link):
+    """
+    Retrieves ticker symbols from a Wikipedia page containing stock exchange information.
+
+    Parameters:
+        link (str): Link to the Wikipedia page containing stock exchange information.
+
+    Returns:
+        List[str]: List of ticker symbols.
+    """
+    tables = pd.read_html(link)
+    df = tables[4]
+    df.drop(['Company', 'GICS Sector', 'GICS Sub-Industry'], axis=1, inplace=True)
+    tickers = df['Ticker'].values.tolist()
+    return tickers
 
 
-### rnn model
+def loaded_df(years, tickers):
+    """
+    Downloads stock price data for the specified number of years and tickers using yfinance.
+    Returns a pandas DataFrame and pickles the data.
+
+    Parameters:
+        years (int): Number of years of historical data to load.
+        tickers (List[str]): List of ticker symbols.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing downloaded stock price data.
+    """
+    stocks_dict = {}
+    time_window = 365 * years
+    start_date = dt.datetime.now() - dt.timedelta(time_window)
+    end_date = dt.datetime.now()
+    for i, ticker in enumerate(tickers):
+        print('Getting {} ({}/{})'.format(ticker, i, len(tickers)))
+        prices = yf.download(ticker, start=start_date, end=end_date)
+        stocks_dict[ticker] = prices['Adj Close']
+
+    stocks_prices = pd.DataFrame.from_dict(stocks_dict)
+    pickle_dump(stocks_prices=stocks_prices)
+    return stocks_prices
+
+
+def clean_df(percentage, tickers, stocks_prices):
+    """
+    Cleans the DataFrame by dropping stocks with NaN values exceeding the given percentage threshold.
+
+    Parameters:
+        percentage (float): Percentage threshold for NaN values.
+        tickers (List[str]): List of ticker symbols.
+        stocks_prices (pandas.DataFrame): DataFrame containing stock prices.
+
+    Returns:
+        pandas.DataFrame: Cleaned DataFrame.
+    """
+    if percentage > 1:
+        percentage = percentage / 100
+    for ticker in tickers:
+        nan_values = stocks_prices[ticker].isnull().values.any()
+        if nan_values:
+            count_nan = stocks_prices[ticker].isnull().sum()
+            if count_nan > (len(stocks_prices) * percentage):
+                stocks_prices.drop(ticker, axis=1, inplace=True)
+    return stocks_prices
+
+
 def xtrain_ytrain(adj_close_df):
-    train_set, test_set = hashing_and_splitting(adj_close_df)
-    sc = MinMaxScaler(feature_range = (0, 1))
+    """
+    Splits the DataFrame into training and testing sets, normalizes the data, and prepares it for LSTM model training.
+
+    Parameters:
+        adj_close_df (pandas.DataFrame): DataFrame containing adjusted close prices.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, MinMaxScaler]: A tuple containing training and testing data along with the scaler.
+    """
+    split_index = int((len(adj_close_df)) * 0.80)
+    train_set = pd.DataFrame(adj_close_df.iloc[0:split_index])
+    test_set = pd.DataFrame(adj_close_df.iloc[split_index:])
+
+    sc = MinMaxScaler(feature_range=(0, 1))
+    sc.fit(train_set)
     training_set_scaled = sc.fit_transform(train_set)
+    test_set_scaled = sc.transform(test_set)
+
     xtrain = []
     ytrain = []
-    for i in range(60, len(adj_close_df)):
-        xtrain.append(training_set_scaled[i-60:i, 0])
-        ytrain.append(training_set_scaled[i, 0]) 
+    for i in range(60, training_set_scaled.shape[0]):
+        xtrain.append(training_set_scaled[i - 60:i, 0])
+        ytrain.append(training_set_scaled[i, 0])
     xtrain, ytrain = np.array(xtrain), np.array(ytrain)
     xtrain = np.reshape(xtrain, (xtrain.shape[0], xtrain.shape[1], 1))
-    return xtrain, ytrain, test_set
+
+    xtest = []
+    ytest = []
+    for i in range(20, test_set_scaled.shape[0]):
+        xtest.append(test_set_scaled[i - 20:i, 0])
+        ytest.append(test_set_scaled[i, 0])
+    xtest, ytest = np.array(xtest), np.array(ytest)
+    return xtrain, ytrain, xtest, ytest, sc
+
 
 def lstm_model(xtrain, ytrain):
+    """
+    Builds and trains an LSTM model using the training data.
+
+    Parameters:
+        xtrain (np.ndarray): Input training data.
+        ytrain (np.ndarray): Target training data.
+
+    Returns:
+        Sequential: Trained LSTM model.
+    """
     model = Sequential()
-    model.add(LSTM(units = 50, return_sequences = True, input_shape = (xtrain.shape[1], 1)))
+    model.add(LSTM(units=50, activation='relu', return_sequences=True, input_shape=(xtrain.shape[1], 1)))
     model.add(Dropout(0.2))
-    model.add(LSTM(units = 50, return_sequences = True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units = 50, return_sequences = True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units = 50))
-    model.add(Dropout(0.2))
-    model.add(Dense(units = 1))
-    #compiling and fitting the model
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
-    model.fit(xtrain, ytrain, epochs = 15, batch_size = 32)
-    return model
-
-def predictions(xtrain, ytrain, xtest):
-    #Making predictions on the test data
-    sc = MinMaxScaler(feature_range = (0, 1))
-    predicted_stock_price = lstm_model(xtrain, ytrain).predict(xtest)
-    real_stock_price = sc.inverse_transform(predicted_stock_price)
-    return predicted_stock_price, real_stock_price
-
-###plotting 
-def visualizing(xtrain, ytrain, xtest):
-    predicted_price, real_price = predictions(xtrain, ytrain, xtest)
-    #Visualizing the prediction
-    plt.figure()
-    plt.plot(real_price, color = 'r', label = 'Close')
-    plt.plot(predicted_price, color = 'b', label = 'Prediction')
-    plt.xlabel('Date')
-    plt.legend()
-    plt.show()
+    model.add(LSTM(units=60, activation='relu', return_sequences=True))
+    model.add
