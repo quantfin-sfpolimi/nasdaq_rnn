865531504fcae73367a5d9787779b6c5f0e044b0
zanisimone <102178824+zanisimone@users.noreply.github.com>
2024-04-04 17:50:12 +0200

    add plotting of correlation martrix of correlated stocks

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ stocks_prices, tickers =    load_dataframe(years=10, filename='cleaned_nasdaq_da
 #if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with average of (t-1) and (t+1)
 stocks_prices = clean_df(10, tickers=tickers, stocks_prices=stocks_prices)
 print(stocks_prices)
+corr_stocks_dict, corr_stocks_list = get_correlated_stocks(stocks_prices, tickers, '2024-03-01 09:30:00', '2024-03-31 15:30:00')
 
+corr_stocks_df = pd.DataFrame()
 
+for ticker in tickers:
+  if ticker in corr_stocks_list:
+    corr_stocks_df[ticker] = stocks_prices[ticker]
 
+plot_corr_matrix(corr_stocks_df ,'2024-03-01 09:30:00', '2024-03-31 15:30:00')
 
