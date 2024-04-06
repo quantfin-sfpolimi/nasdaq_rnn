470721dce646534cafe727a53da81f18abb41c3a
giutala <giuliatala32@gmail.com>
2024-04-06 11:35:59 +0200

    add functions to return correlatedstocks_df

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from openbb import obb
 import pandas as pd
-from helpers import load_dataframe, clean_df, get_correlated_stocks, plot_corr_matrix
+from helpers import load_dataframe, clean_df, get_correlated_stocks, plot_corr_matrix, corr_df
 
 # simo's login with obb platform credetial
 obb.account.login(email='simo05062003@gmail.com', password='##2yTFb2F4Zd9z')
 #load data, clean data frame (closing stock prices)
 stocks_prices, tickers = load_dataframe(years=10, filename='cleaned_nasdaq_dataframe', link='https://en.wikipedia.org/wiki/Nasdaq-100', interval='5m') 
-#if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with average of (t-1) and (t+1)
+#if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with (t-1)
 stocks_prices = clean_df(10, tickers=tickers, stocks_prices=stocks_prices)
 
 corr_stocks_dict, corr_stocks_list = get_correlated_stocks(stocks_prices, tickers, '2024-03-01 09:30:00', '2024-03-31 15:30:00')
 
-corr_stocks_df = pd.DataFrame()
+corr_stocks_df = corr_df(corr_stocks_dict, corr_stocks_list, tickers=tickers, stocks_prices=stocks_prices)
 
-for ticker in tickers:
-  if ticker in corr_stocks_list:
-    corr_stocks_df[ticker] = stocks_prices[ticker]
-
-plot_corr_matrix(corr_stocks_df ,'2024-03-01 09:30:00', '2024-03-31 15:30:00')
+plot_corr_matrix(corr_stocks_df)
