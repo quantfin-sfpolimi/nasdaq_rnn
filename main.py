ac651f254f49caf675b4131abd93e93284ac5d2b
Giulio Tonielli <70532912+giuliotonielli@users.noreply.github.com>
2024-04-12 11:03:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from openbb import obb
 import pandas as pd
 from helpers import DataFrameHelper, CorrelationAnalysis
 
+dataFrame_nasdaq = DataFrameHelper(filename='cleaned_nasdaq_dataframe', link='https://en.wikipedia.org/wiki/Nasdaq-100', years=10, interval='5m')
+
 # simo's login with obb platform credetial
 obb.account.login(email='simo05062003@gmail.com', password='##2yTFb2F4Zd9z')
+
 #load data, clean data frame (closing stock prices)
-stocks_prices, tickers = DataFrameHelper.load_dataframe(years=10, filename='cleaned_nasdaq_dataframe', link='https://en.wikipedia.org/wiki/Nasdaq-100', interval='5m') 
+dataFrame_nasdaq.load()
+
 #if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with (t-1)
-stocks_prices = DataFrameHelper.clean_df(10, tickers=tickers, stocks_prices=stocks_prices)
+dataFrame_nasdaq.clean_df(10)
 
-corr_stocks_dict, corr_stocks_list = CorrelationAnalysis.get_correlated_stocks(stocks_prices, tickers, '2024-03-01 09:30:00', '2024-03-31 15:30:00')
+corr_analyses_nasdaq = CorrelationAnalysis(prices=dataFrame_nasdaq.prices, tickers=dataFrame_nasdaq.tickers, start_datetime='2024-03-01 09:30:00', end_datetime='2024-03-31 15:30:00')
+corr_stocks_dict, corr_stocks_list = corr_analyses_nasdaq.get_correlated_stocks()
 
-corr_stocks_df = CorrelationAnalysis.corr_df(corr_stocks_dict, corr_stocks_list, tickers=tickers, stocks_prices=stocks_prices)
+corr_analyses_nasdaq.df = corr_analyses_nasdaq.corr_df(corr_stocks_dict, corr_stocks_list)
 
-CorrelationAnalysis.plot_corr_matrix(corr_stocks_df)
-print("GO")
+corr_analyses_nasdaq.plot_corr_matrix()
