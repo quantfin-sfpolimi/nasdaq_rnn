48b541e4302a716048db13986f5d60a80d0352bd
zanisimone <102178824+zanisimone@users.noreply.github.com>
2024-04-13 16:11:03 +0200

    udate main

--- main.py
+++ main.py
@@ -8,19 +8,13 @@ dataFrame_nasdaq = DataFrameHelper(filename='cleaned_nasdaq_dataframe', link='ht
 obb.account.login(email='simo05062003@gmail.com', password='##2yTFb2F4Zd9z')
 
 #load data, clean data frame (closing stock prices)
-stocks_prices, tickers =    load_dataframe(years=10, filename='cleaned_nasdaq_dataframe', link='https://en.wikipedia.org/wiki/Nasdaq-100', interval='5m') 
-#if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with average of (t-1) and (t+1)
-stocks_prices = clean_df(10, tickers=tickers, stocks_prices=stocks_prices)
-print(stocks_prices)
-corr_stocks_dict, corr_stocks_list = get_correlated_stocks(stocks_prices, tickers, '2024-03-01 09:30:00', '2024-03-31 15:30:00')
+dataFrame_nasdaq.load()
 
-corr_stocks_df = pd.DataFrame()
+#if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with (t-1)
+dataFrame_nasdaq.clean_df(10)
 
-for ticker in tickers:
-  if ticker in corr_stocks_list:
-    corr_stocks_df[ticker] = stocks_prices[ticker]
-
-plot_corr_matrix(corr_stocks_df ,'2024-03-01 09:30:00', '2024-03-31 15:30:00')
+corr_analyses_nasdaq = CorrelationAnalysis(prices=dataFrame_nasdaq.prices, tickers=dataFrame_nasdaq.tickers, start_datetime='2024-03-01 09:30:00', end_datetime='2024-03-31 15:30:00')
+corr_stocks_dict, corr_stocks_list = corr_analyses_nasdaq.get_correlated_stocks()
 
 corr_analyses_nasdaq.df = corr_analyses_nasdaq.corr_df(corr_stocks_dict, corr_stocks_list)
 
