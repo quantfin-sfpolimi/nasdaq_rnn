190ad30c6646386b9411561704cad4d3187b2bda
giutala <giuliatala32@gmail.com>
2024-05-26 19:37:44 +0200

    rework 5k batching of  helpermodules
    Co-authored-by: @Maarrk

---
+++ df_cleaning_test.py
@@ -0,0 +1,33 @@
+from helpermodules.df_cleaning import Timestamping
+from datetime import datetime, timedelta
+
+start = datetime(year=2022, month=11, day=29)
+end = datetime(year=2022, month=11, day=30, hour= 23)
+lista = [t for t in Timestamping(start, end)]
+#print(lista[0], lista[-1])
+print(len(lista))
+
+generator = Timestamping(start_date=start, end_date=end)
+boundaries = []
+try:
+    boundary_start = next(generator)
+    boundary_end = boundary_start 
+    while True: 
+        for _ in range(9):
+            try:
+                boundary_end = next(generator)
+            except StopIteration:
+                boundaries.append((boundary_start, boundary_end))
+                raise StopIteration
+        boundaries.append((boundary_start, boundary_end))
+        boundary_start = next(generator)   
+except StopIteration:
+    pass
+
+for b in boundaries:
+    print(b)
+    
+
+end = datetime.now()
+start = end - timedelta(days=61)
+print('Length for 2 months:', len([t for t in Timestamping(start, end)]))
